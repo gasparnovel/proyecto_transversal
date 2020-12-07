a4b742221f77d52498ec1ff3fd2a761ec7ec9dbd
Alejnadro Lopez <alopezmur@cifpfbmoll.eu>
2020-12-07 20:48:09 +0100

    feat: :sparkles: devolver key en lista

--- Back-End/src/make_diccionary.py
+++ Back-End/src/make_diccionary.py
@@ -3,13 +3,23 @@ from remove_html import productos_sin_hmtl
 
 
 def get_keys(texto):
-    if ':' in texto:
-        texto = ''
-        return texto
-    else:
-        return texto
+    keys = []
+    for item in texto:
+        if ':' == item:
+            item = ''
+            return item
+        if ':' in item:
+            separador = item.find(':')
+            item = item[0:separador]
+            keys.append(item)
+            return keys
+        else:
+            return item
 
 
 if __name__ == "__main__":
-    assert get_keys(':') == ('')
-    assert get_keys('h') == ('h')
+    assert get_keys(':') == ''
+    assert get_keys('h') == 'h'
+    assert get_keys('h:') == 'h'
+    assert get_keys('h: a') == 'h'
+    assert get_keys(['h:']) == ['h']
