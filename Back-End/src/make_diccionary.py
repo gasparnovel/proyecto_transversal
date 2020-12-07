cffd4b14fc46467371b382c41f385baef854c202
Alejnadro Lopez <alopezmur@cifpfbmoll.eu>
2020-12-07 23:03:12 +0100

    test: :white_check_mark: añadir caso test make_dictionary

--- Back-End/src/make_diccionary.py
+++ Back-End/src/make_diccionary.py
@@ -4,17 +4,28 @@ from remove_html import productos_sin_hmtl
 
 def get_keys(texto):
     keys = []
+    dos_puntos = ':'
     for item in texto:
         if ':' == item:
             item = ''
             return item
-        if ':' in item:
+        if item.count(dos_puntos) == 1 and len(texto) == 1:
             separador = item.find(':')
             item = item[0:separador]
             keys.append(item)
             return keys
+        if item.count(dos_puntos) == 1:
+            separador = item.find(':')
+            item = item[0:separador]
+            keys.append(item)
+            continue
+        if item.count(dos_puntos) > 1:
+            separador = item.find(':')
+            key = item[0:separador]
+            keys.append(key)
         else:
             return item
+    return keys
 
 
 if __name__ == "__main__":
@@ -23,3 +34,5 @@ if __name__ == "__main__":
     assert get_keys('h:') == 'h'
     assert get_keys('h: a') == 'h'
     assert get_keys(['h:']) == ['h']
+    assert get_keys(['h:', 'a:']) == ['h', 'a']
+    assert get_keys([' hola: que tal marca: droide']) == ['hola', 'marca']
