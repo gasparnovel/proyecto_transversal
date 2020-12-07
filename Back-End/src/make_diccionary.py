47d2a6a3ad691ed7d29b4d63602f701a17dc41eb
Alejnadro Lopez <alopezmur@cifpfbmoll.eu>
2020-12-07 20:36:34 +0100

    feat: :sparkles: devovler vacio

--- Back-End/src/make_diccionary.py
+++ Back-End/src/make_diccionary.py
@@ -1,2 +1,11 @@
 import urllib
 from remove_html import productos_sin_hmtl
+
+
+def get_keys(texto):
+    texto = ''
+    return texto
+
+
+if __name__ == "__main__":
+    assert get_keys(':') == ('')
