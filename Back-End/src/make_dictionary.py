92853cd96fb247a6796af3cb385cc559ce71ec48
Alejnadro Lopez <alopezmur@cifpfbmoll.eu>
2020-12-08 10:33:53 +0100

    feat: :sparkles: make_dictionary devuelve texto

--- Back-End/src/make_dictionary.py
+++ Back-End/src/make_dictionary.py
@@ -5,7 +5,10 @@ from remove_html import productos_sin_hmtl
 def make_dictionary(lista):
     if lista == ':':
         return ''
+    else:
+        return lista
 
 
 if __name__ == "__main__":
     assert make_dictionary(':') == ''
+    assert make_dictionary('h') == 'h'
