e0a24457ddc061a59cd91a16c79504c3e6dcf53e
gasparnovel <gnovel@cifpfbmoll.eu>
2020-12-09 00:13:31 +0100

    feat: :sparkles: Carpeta repository

---
+++ Back-End/repository/db_atlas.py
@@ -0,0 +1,23 @@
+
+
+
+import pymongo
+from make_dictionary import make_dictionary
+
+def make_db():
+    myclient = pymongo.MongoClient("mongodb+srv://administrador:go-go-ufo12345@cluster0.scjfs.mongodb.net/admin")
+    mydb = myclient["mydatabase"]
+    
+    mycol = mydb["customers"]
+
+    mylist = make_dictionary
+
+    x = mycol.insert_many(mylist)
+
+    #print list of the _id values of the inserted documents:
+    print(x.inserted_ids)
+
+def verification():
+    collist = mydb.list_collection_names()
+        if "customers" in collist:
+    print("The collection exists.")
